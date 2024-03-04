ee3e5cc1b3ac28b8e8253696b501c73599da3631
EasChen70 <eason_chen@uri.edu>
2024-03-03 20:32:59 -0500

    added documentation + crediting sources

--- src/jqueryAjax.js
+++ src/jqueryAjax.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
+    //jquery select button + add event listener
     $("#bookingbutton").on('click', function(){
+        //this makes sure that there is only one instance of a data form at a time
         if ($(".data-form").length === 0){
+        //create dataform div
         const dataForm = $("<div>");
+        //add class
         dataForm.addClass("data-form");
+        //css styling taken from the htmlajax.js
         dataForm.css({
             "height": "500px",
             "width": "600px",
@@ -15,15 +20,19 @@ $(document).ready(function(){
             "border-radius": "2%",
             "overflow": "auto"
         }).on('click', function(){
+            //click on dataform to exit
             $(this).remove();
         });
+        //insert
         $(this).before(dataForm);
+        //loaddata
         loadData('data/data2.html');
     }
     
 });
 
     function loadData(filepath){
+        //provide filepath, type of request, and what to do with successful response, got help from w3 schools
         $.ajax({url: filepath, type: 'GET', success: function(response){
             $('.data-form').html(response);
         }})
